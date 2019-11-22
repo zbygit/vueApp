18b3bd8d81d33c24bd7523d9c312a2945639ee61
YZN0816 <819687166@qq.com>
2019-11-22 16:29:11 +0800

    商品详情

---
+++ src/components/home/detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <top>
+       <div slot="center">{{msg}}</div>
+       <div slot="left">返回</div>
+       <div slot="right">{{this.$route.query.index}}</div>
+    </top>
+  </div>
+</template>
+<script>
+import top from "../../publiccomponent/topNav"
+export default {
+  name: "detail",
+  data(){
+    return {
+      msg:"商品详情"
+    }
+  },
+  components: {
+    top
+  }
+}
+</script>
+<style scoped>
+</style>
