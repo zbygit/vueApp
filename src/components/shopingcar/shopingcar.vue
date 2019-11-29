b954c1f41029f4d4b15aec871974329b3ff958ae
chen123 <446939508@qq.com>
2019-11-29 13:21:48 +0800

    123

--- src/components/shopingcar/shopingcar.vue
+++ src/components/shopingcar/shopingcar.vue
@@ -7,20 +7,18 @@
       <ul>
         <li v-for="(item,index) of bookhotList" :key="index">
           <img :src="item.bookImg" :alt="item.bookImg" />
-
-          <p>{{item.bookName}}</p>
-
-          <p class="price">{{item.bookPrice}}</p>
-
-          <p>
-            <button @click="numjianjian(item.bookId)">-</button>
-            {{ item.num}}
-            <button @click="numjiajia(item.bookId)">+</button>
-          </p>
-
-          <p>
-            <button type="button" @click="remove(index)">删除</button>
-          </p>
+          <div>
+            <p>{{item.bookName}}</p>
+            <span class="price">{{item.bookPrice}}</span>
+            <span class="mn1">
+              <button @click="numjianjian(item.id)">-</button>
+              {{ item.num}}
+              <button @click="numjiajia(item.id)">+</button>
+            </span>
+            <span class="mn2">
+              <button type="button" @click="remove(index)">删除</button>
+            </span>
+          </div>
         </li>
       </ul>
     </div>
@@ -36,15 +34,16 @@
 import topNav from "../../publiccomponent/topNav";
 import bottom from "../../publiccomponent/bottomNav";
 import { gethotlist } from "../../baseapi";
+import Cookies from "js-cookie";
 export default {
   name: "shopingcar",
   data() {
     return {
       pageMsg: "购物车",
       bookhotList: null,
-
       totalP: 0
-    };
+     
+   };
   },
 
   components: { topNav, bottom },
@@ -53,30 +52,45 @@ export default {
   },
   methods: {
     numjiajia(id) {
-      console.log(id);
-
-      console.log(this.bookhotList);
-      this.bookhotList[id].num += 1;
+      this.bookhotList.forEach(element => {
+         if(element.id==id){
+            element.num+=1;
+         }
+      });
       this.totalP = this.bookhotList.reduce(function(current, next, index) {
+     
         return current + next.bookPrice * next.num;
       }, 0);
     },
     numjianjian(id) {
-      if (this.bookhotList[id].num <= 0) {
-        this.bookhotList[id].num = 0;
-      } else {
-        this.bookhotList[id].num -= 1;
-      }
+       
+       
+       this.bookhotList.forEach(element => {
+         if(element.id==id){
+            element.num-=1;
+         }else if(element.id<=0){
+              element. num =0;
+        }
+     
+     
+     });
+       
+
+
+
       this.totalP = this.bookhotList.reduce(function(current, next, index) {
         return current + next.bookPrice * next.num;
       }, 0);
     },
 
     getl() {
-      gethotlist().then(res => {
-        console.log(res);
-        this.bookhotList = res.hotlist;
-      });
+      // gethotlist().then(res => {
+      //   console.log(res);
+   let countCookie = Cookies.get("car");
+      this.bookhotList =
+        countCookie == undefined ? [] : JSON.parse(countCookie);
+
+       
     },
     remove(inx) {
       this.bookhotList = this.bookhotList.filter((bookhotList, index) => {
@@ -85,19 +99,24 @@ export default {
     }
   }
 };
+     
+
+
+
+
 </script>
 
 // <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
 @border-botm: 1px solid #f5f5f5;
 .hello {
-  margin-bottom: 100px;
+  margin-bottom: 80px;
 }
 .book {
   width: 90%;
   margin: 4rem 0 6rem;
   background: #fff;
-  // position:relative;
+
   height: calc(100% - 10rem);
   ul {
     display: flex;
@@ -114,25 +133,40 @@ export default {
         white-space: nowrap;
 
         color: #232326;
-        font-size: 18px;
+        font-size: 24px;
         padding-bottom: 38px;
       }
-      // p:nth-of-type(1) {
-      // position: relative
-      // left：-30px;
-
-      // }
-
-      input button {
-        width: 40px;
-        height: 20px;
+     
+     
+      .mn1{
+        width:40px;
+        height:40px;
+        font-size:25px;  
+        margin:20px;
+       
+     }
+     
+     button{
+       width:40px;height:40px
+     }
+     
+     
+     .mn2{
+      width:40px;
+     margin: 20px;
+      font-size:25px; 
+     
+     }
+      .mn2  button{
+          width:40px;height:40px
       }
+    
 
       .price {
-        color: red;
-        font-size: 19px;
-        position: relative;
-        left: -30px;
+        margin:20px; 
+       color: red;
+       
+      font-size: 25px;
       }
       &:nth-child(even) {
         border-left: @border-botm;
